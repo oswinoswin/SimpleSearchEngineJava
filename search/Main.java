586630b67a50158761e829db3aaf10a480687130
oswinoswin <laura.uchwat@gmail.com>
2023-06-07 14:13:29 +0200

    Stage 2

--- search/Main.java
+++ search/Main.java
@@ -1,12 +1,60 @@
 package search;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+class SimpleSE{
+    ArrayList<String> peopleList;
+
+    public SimpleSE() {
+        this.peopleList = new ArrayList<>();
+    }
+
+    public void addToList(String person){
+        this.peopleList.add(person);
+    }
+
+    ArrayList<String> matchingPeople(String query){
+        query = query.strip().toLowerCase();
+        ArrayList<String> matching = new ArrayList<>();
+        for (String person: peopleList){
+            if(person.toLowerCase().contains(query)){
+                matching.add(person);
+            }
+        }
+        return matching;
+    }
+
+    public void printMatchingPeople(String query){
+        ArrayList<String> matching = this.matchingPeople(query);
+        if (matching.isEmpty()){
+            System.out.println("No matching people found");
+        }else{
+            System.out.println("Found people:");
+            for (String person: matching){
+                System.out.println(person);
+            }
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine().strip();
-        String searchedWord = scanner.next();
-        System.out.println(searchForWord(line, searchedWord));
+        SimpleSE simpleSE = new SimpleSE();
+        System.out.println("Enter the number of people:");
+        int N = scanner.nextInt();
+        System.out.println("Enter all the people:");
+        for (int i=0; i<=N; i++){
+            String person = scanner.nextLine().trim();
+            simpleSE.addToList(person);
+        }
+        System.out.println("Enter the number of search queries:");
+        int M = scanner.nextInt();
+        for(int i=0; i<M; i++){
+            System.out.println("Enter data to search people:");
+            String query = scanner.next();
+            simpleSE.printMatchingPeople(query);
+        }
+
     }
     public static String searchForWord(String line, String searchedWord){
         line = line.trim().strip();
